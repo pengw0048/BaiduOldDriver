c208a70a5a9a5bd09921f2bb8c460fddc928542d
伪红学家 <pengwang@cmu.edu>
2016-12-25 16:46:37 -0800

    Separate test code.

--- BaiduOldDriver/Test/Test.cs
+++ BaiduOldDriver/Test/Test.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NetDisk;
 using System.IO;
 
-namespace NetDisk
+namespace Test
 {
     public class Test
     {
