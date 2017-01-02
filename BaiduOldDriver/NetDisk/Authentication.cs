182e25cbdcc96afacebfeb7267c75aacd801b0ff
伪红学家 <pengwang@cmu.edu>
2017-01-02 21:14:42 +0800

    Clean up.

--- BaiduOldDriver/NetDisk/Authentication.cs
+++ BaiduOldDriver/NetDisk/Authentication.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace NetDisk
 {
--- BaiduOldDriver/NetDisk/Extension.cs
+++ BaiduOldDriver/NetDisk/Extension.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using System.Linq;
 using System.Net;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NetDisk
 {
